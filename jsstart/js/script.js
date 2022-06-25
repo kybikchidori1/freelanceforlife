2d0122905f26d6346a2d96255138ac140f3a9621
Roman <kybik_rybik1994@mail.ru>
2022-06-25 13:58:18 +0300

    2_video

--- jsstart/js/script.js
+++ jsstart/js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+// ========================= 1 video ========================
+
+// console.log("Hello world!");
+
+// ========================= 2 video ========================
+
+// "use strict";
+
+// console.log("Я учу JS");
