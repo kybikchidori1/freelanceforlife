c4eb0fed4abf804ef0aa9770730cb680a1d5a663
Roman <kybik_rybik1994@mail.ru>
2022-06-29 18:22:18 +0300

    video_7

--- jsstart/js/script.js
+++ jsstart/js/script.js
@@ -24,21 +24,50 @@
 
 // ========================= 5 video ========================
 
-let a = "35" + -"22";
+// let a = "35" + -"22";
 
-console.log(a); //35-22
-console.log(typeof a);
+// console.log(a); //35-22
+// console.log(typeof a);
 
-console.log('35' * "22") //770
+// console.log('35' * "22") //770
 
-console.log('558' > 22++) //error
+// console.log('558' > 22++) //error
 
+// let usersCounter = 0
+// let newUsers = usersCounter++
+// console.log(newUsers) //true
 
-let usersCounter = 0
-let newUsers = usersCounter++
-console.log(newUsers) //true
+// console.log(!false && 11 || 18 && !'') // 11 || 0 // 11
 
-console.log(!false && 11 || 18 && !'') // 11 || 0 // 11
+// let name = 0
+// console.log(name ?? "Без имени") // 0
 
-let name = 0
-console.log(name ?? "Без имени") // 0
+// ========================= 7 video ========================
+
+// let num = 1;
+// while (num < 6) {
+//   console.log(num);
+//   num++;
+// } // 1, 2, 3, 4, 5
+
+// let num = 8;
+// while (num) {
+//   console.log(num);
+//   num--;
+// } // 8, 7, 6, 5, 4, 3, 2, 1
+
+// let num = 0;
+
+// while (num < 3) {
+//   console.log(`Число ${num}`);
+//   num++;
+// }
+
+// firstFor: for (let num = 0; num < 2; num++) {
+//   for (let size = 0; size < 3; size++) {
+//     if (size == 1) {
+//       break firstFor;
+//     }
+//     console.log(size);
+//   }
+// }
